da3368934f3eb4367eca1015808544a17047e71c
xenon <arcon_ark@protonmail.com>
2025-04-07 17:48:26 +0200

    Course information step 7

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -3,12 +3,13 @@ import Header from './Header'
 import Content from './Content'
 
 const Course = ({course}) => {
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      <p><b>TOTAL OF {} EXCERCISES</b></p>
+      <p><b>TOTAL OF {totalExercises} EXCERCISES</b></p>
     </>
   )
 }
