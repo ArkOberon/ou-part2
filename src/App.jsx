1d9dc4f38d5f6fdc8bc526a031f17406a9ed95a0
xenon <pmcruz@hermenautas.com>
2025-04-07 16:10:58 +0200

    2.1: Course information step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+import Course from "./components/Course"
+import { course } from './constant'
 
+const App = () => {  
   return <Course course={course} />
 }
 
---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {
+        parts.map((part) => (
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>
+        ))
+      }
+    </>
+  )
+}
+
+export default Content
